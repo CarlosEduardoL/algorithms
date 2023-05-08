edc86019e88c74073e5d8be71ebce3d3fd728ec0
Carlos Eduardo Lizalda Valencia <36455436+CarlosEduardoL@users.noreply.github.com>
2023-05-08 12:30:26 -0500

    Fix bad refactor typos on list rust

--- rust/src/collections/linear/list.rs
+++ rust/src/collections/linear/list.rs
@@ -1,7 +1,7 @@
 use std::ops::{Index, IndexMut};
 
 pub trait List<T>: Index<usize, Output=T> + IndexMut<usize, Output=T> {
-    /// Returns the element at a specific index in the linear.
+    /// Returns the element at a specific index in the list.
     ///
     /// # Arguments
     ///
@@ -19,47 +19,47 @@ pub trait List<T>: Index<usize, Output=T> + IndexMut<usize, Output=T> {
         }
     }
 
-    /// Adds an element to the front of the linear.
+    /// Adds an element to the front of the list.
     ///
     /// # Arguments
     ///
-    /// * `data` - The data to add to the front of the linear.
+    /// * `data` - The data to add to the front of the list.
     fn push_front(&mut self, data: T);
 
-    /// Adds an element to the back of the linear.
+    /// Adds an element to the back of the list.
     ///
     /// # Arguments
     ///
-    /// * `data` - The data to add to the back of the linear.
+    /// * `data` - The data to add to the back of the list.
     fn push_back(&mut self, data: T);
 
-    /// Removes and returns the first element of the linear.
+    /// Removes and returns the first element of the list.
     ///
     /// # Returns
     ///
-    /// An option containing the first element of the linear,
-    /// or None if the linear is empty.
+    /// An option containing the first element of the list,
+    /// or None if the list is empty.
     fn pop_front(&mut self) -> Option<T>;
 
-    /// Removes and returns the last element of the linear.
+    /// Removes and returns the last element of the list.
     ///
     /// # Returns
     ///
-    /// An option containing the last element of the linear,
-    /// or None if the linear is empty.
+    /// An option containing the last element of the list,
+    /// or None if the list is empty.
     fn pop_back(&mut self) -> Option<T>;
 
-    /// Returns the number of elements in the linear.
+    /// Returns the number of elements in the list.
     ///
     /// # Returns
     ///
-    /// The number of elements in the linear.
+    /// The number of elements in the list.
     fn len(&self) -> usize;
 
-    /// Checks if the linear is empty.
+    /// Checks if the list is empty.
     ///
     /// # Returns
     ///
-    /// True if the linear is empty, false otherwise.
+    /// True if the list is empty, false otherwise.
     fn is_empty(&self) -> bool;
 }
